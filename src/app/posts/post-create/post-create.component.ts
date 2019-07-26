2ec0faffe3eab3ce23f0f6812ffd2f90af4d630f
Evan Thacker <thackerevan@gmail.com>
2019-07-25 21:19:16 -0400

    warning: LF will be replaced by CRLF in src/app/app-routing.module.ts. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/app/app.component.html. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/app/app.component.spec.ts. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/app/app.component.ts. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/app/app.module.ts. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/browserslist. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/environments/environment.prod.ts. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/environments/environment.ts. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/index.html. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/karma.conf.js. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/main.ts. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/polyfills.ts. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/styles.css. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/test.ts. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/tsconfig.app.json. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/tsconfig.spec.json. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/tslint.json. The file will have its original line endings in your working directory

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,24 @@
+import { PostsService } from './../posts.service';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+  enteredContent = '';
+  enteredTitle = '';
+
+  constructor(public postsService: PostsService) {}
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+  this.postsService.addPosts(form.value.title, form.value.content);
+  form.resetForm();
+  }
+}
